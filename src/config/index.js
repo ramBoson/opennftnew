cf632ab691c41d8eeb46d571929596107a78a882
ramBoson <ramamugesh95@gmail.com>
2021-09-17 01:30:21 +0530

    add url

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,10 @@
 export const ALGORAND_LEDGER = 'TestNet';
 export const USDC_ID = 14001707;
 export const USDC_DECIMAL_POINTS = 2;
-export const ASSET_URL = 'nft-market.ulam.io';
+export const ASSET_URL = 'https://34.121.240.235/';
 export var BACKEND_URL;
 if (process.env.NODE_ENV === 'development') {
-  BACKEND_URL = 'http://localhost:8000';
+  BACKEND_URL = 'https://34.121.240.235/';
 } else {
-  BACKEND_URL = 'https://nft-be.ulam.io';
+  BACKEND_URL = 'https://34.121.240.235/';
 }
