4bc17c9a19ad1dff276c34f00bb78ad4087625b9
ramBoson <ramamugesh95@gmail.com>
2021-09-18 13:31:10 +0530

    remove s in indexfiel

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,10 @@
 export const ALGORAND_LEDGER = 'TestNet';
 export const USDC_ID = 14001707;
 export const USDC_DECIMAL_POINTS = 2;
-export const ASSET_URL = 'https://34.121.240.235/';
+export const ASSET_URL = 'http://34.121.240.235';
 export var BACKEND_URL;
 if (process.env.NODE_ENV === 'development') {
-  BACKEND_URL = 'https://34.121.240.235/';
+  BACKEND_URL = 'http://34.121.240.235';
 } else {
-  BACKEND_URL = 'https://34.121.240.235/';
+  BACKEND_URL = 'http://34.121.240.235';
 }
